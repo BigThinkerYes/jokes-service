40690ef1a7fbd3d0c1c41f9b913c88fe5e374da5
BigThinkerYes <designaspnet4@hotmail.com>
2023-01-19 15:43:51 -0600

    bonus -- task

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { Joke } = require('./db');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+//-----------------------------------------------------
 app.get('/jokes', async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
@@ -28,6 +29,53 @@ app.get('/jokes', async (req, res, next) => {
     next(error)
   }
 });
+//-----------------------------------------------------
+app.post('/jokes', async (req, res, next) => {
+  try{
+    const {joke, tag}= req.body; //UI form
+    const jokes = await Joke.create({joke,tag})
+    res.json({message: 'success', jokes})
+  }catch (error){
+    console.log(error.message);
+    next(error);
+  }
 
+});
+//-----------------------------------------------------
+app.delete('/jokes/:id', async (req, res, next) => {
+  try{
+    const id = req.params.id;
+    const jokes = await Joke.findByPk(id);
+    if(!jokes){
+      res.status(404).json({message: `Joke not found ${id}.`});
+      await Joke.destroy({where:{id}});
+      res.status(200).json({message: 'Jokes Successful ', jokes});
+    }
+  } catch(error){
+    console.log(error.message);
+    next(error);
+  }
+
+});
+//-----------------------------------------------------
+app.put('/jokes/:id', async (req, res, next) => {
+  try{
+    const id = req.params.id;
+    const jokes = await Joke.findAll();
+    
+   
+    if(!jokes){
+      res.status(404).json({message: `Joke could not be updated ${id}.`});
+      await Joke.update({where:{id}});
+      res.status(200).json({message: 'Jokes Successful update ', jokes});
+    }
+  } catch(error){
+    console.log(error.message);
+    next(error);
+  }
+
+});
+
+//-----------------------------------------------------
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
