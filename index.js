2b766ffff73fd92e0f241955daf228f09f33c21c
BigThinkerYes <designaspnet4@hotmail.com>
2023-01-19 14:25:55 -0600

    test-passed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Op } = require('sequelize');
 const app = express();
 const { Joke } = require('./db');
 
@@ -8,7 +9,19 @@ app.use(express.urlencoded({extended:true}));
 app.get('/jokes', async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const where = {}
+    if (req.query.tags){
+      where.tags={
+        [Op.like] : `%${req.query.tags}%`
+      }
+    }
+    if (req.query.content){
+      where.joke =  {
+        [Op.like]: `%${req.query.content}%`,
+      };
+    }
+   const jokes = await Joke.findAll({ where });
+    // const jokes = [];
     res.send(jokes);
   } catch (error) {
     console.error(error);
